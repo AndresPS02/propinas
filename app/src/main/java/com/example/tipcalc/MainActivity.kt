0246b1edc4774a7f6b0aeae2d91f110291a9d126
andres <A01654636@itesm.mx>
2022-03-01 21:59:22 -0600

    Commit Final

--- app/src/main/java/com/example/tipcalc/MainActivity.kt
+++ app/src/main/java/com/example/tipcalc/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import com.example.tipcalc.databinding.ActivityMainBinding
@@ -13,61 +14,64 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUI()
-        setContentView(R.layout.activity_main)
+    }
+
+    private fun initUI() {
         val etiqueta = findViewById<TextView>(R.id.button15)
-        val buton = findViewById<Button>(R.id.button15)
         val etiqueta1 = findViewById<TextView>(R.id.button25)
-        val buton1 = findViewById<Button>(R.id.button25)
         val etiqueta2 = findViewById<TextView>(R.id.button35)
-        val buton2 = findViewById<Button>(R.id.button35)
-        buton.setOnClickListener{
+        binding.button15.setOnClickListener {
+            interfaceCalculateTip(0.15)
             etiqueta.text = "Le agradecemos su preferencia"
             Log.v("edu.itesm.mensaje", "test de boton propina")
             Toast.makeText(this, "Gracias por su propina", Toast.LENGTH_LONG).show()
         }
-        buton1.setOnClickListener{
+
+        binding.button25.setOnClickListener {
+            interfaceCalculateTip(0.25)
             etiqueta1.text = "Le agradecemos su preferencia"
             Log.v("edu.itesm.mensaje", "test de boton propina")
             Toast.makeText(this, "Muchas gracias por su propina", Toast.LENGTH_LONG).show()
+
         }
-        buton2.setOnClickListener{
+
+        binding.button35.setOnClickListener {
+            interfaceCalculateTip(0.35)
             etiqueta2.text = "Le agradecemos su preferencia"
             Log.v("edu.itesm.mensaje", "test de boton propina")
             Toast.makeText(this, "Dios le bendiga", Toast.LENGTH_LONG).show()
         }
 
 
-
-    }
-
-    private fun initUI() {
-        binding.button15.setOnClickListener {
-            interfaceCalculateTip(0.15)
-        }
-
-        binding.button25.setOnClickListener {
-            interfaceCalculateTip(0.25)
-        }
-
-        binding.button35.setOnClickListener {
-            interfaceCalculateTip(0.35)
-        }
-
-
     }
 
     private fun interfaceCalculateTip(percentage: Double) {
-        val amount = binding.editTextNumberDecimal.text.toString()
+        val amount = binding.amountValue.text.toString()
         val amountValue = amount.toDoubleOrNull()
+
         if (amountValue == null) {
             binding.totalTip.text = "0.0"
             binding.totalAmount.text = "0.0"
             return
         }
+        if(amountValue <= 0){
+            binding.amountValue.setOnClickListener{
+                Log.v("edu.itesm.mensaje", "test de boton error")
+                Toast.makeText(this, "Error: ha introducido una cifra inválida, seleccione una cifra mayor a 0", Toast.LENGTH_LONG).show()
+            }
+            return
+        }
         binding.totalTip.text = "${amountValue * percentage}"
         binding.totalAmount.text = "${amountValue * (1 + percentage)}"
+        binding.switch1.setOnCheckedChangeListener { compoundButton, b ->
+            if (true) {
+                binding.totalAmount.text = "${kotlin.math.round(amountValue * (1 + percentage))}"
+            }
+        }
+
     }
 }
